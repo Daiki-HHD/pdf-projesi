a923420a2fe1bb41935690ffead738124b8fd7c4
Daiki <hasanhuseyinderi755@gmail.com>
2026-07-13 15:28:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Yüklediğin ikonu uygulamaya tanıtan yeni yol
+@app.route('/icon.png')
+def serve_icon():
+    return send_file('icon.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
